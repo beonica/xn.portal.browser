1e61b7790928ff2d0e6deaa5dc4c9b9c3006ac74
Thiago F <thiago@usebeon.com>
2018-09-18 19:38:03 -0300

    Mapeando nomes dos clientes aos seus códigos.

--- src/core/main.mjs
+++ src/core/main.mjs
@@ -1,6 +1,10 @@
 import config from "../config.mjs";
 
-export const clientKeys = new Map([["23ae4ed5", "Ocean Drop"]]);
+export const clientKeys = new Map([
+  ["a3946eb8", "Casa Mind"],
+  ["c1a1cdb5", "Ludi"],
+  ["23ae4ed5", "Ocean Drop"]
+]);
 
 export const loadPortal = async ({ client }) => {
   const response = await fetch(`${config.backend}${client}/portal`, {
